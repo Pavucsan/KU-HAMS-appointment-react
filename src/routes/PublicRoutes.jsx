ae5086468cdc9589b2b9e98780e38556a1e05aee
Pavucs Manokar <pavucsan@smartzi.com>
2025-04-23 01:13:58 +0530

    Initial commit

---
+++ src/routes/PublicRoutes.jsx
@@ -0,0 +1,56 @@
+import { lazy } from 'react';
+
+// project imports
+import Loadable from 'components/Loadable';
+import PublicLayout from 'layout/Public';
+
+// render- Dashboard
+const DashboardDefault = Loadable(lazy(() => import('pages/dashboard/default')));
+
+// render - color
+const Color = Loadable(lazy(() => import('pages/component-overview/color')));
+const Typography = Loadable(lazy(() => import('pages/component-overview/typography')));
+const Shadow = Loadable(lazy(() => import('pages/component-overview/shadows')));
+
+// render - sample page
+const SamplePage = Loadable(lazy(() => import('pages/extra-pages/sample-page')));
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const PublicRoutes = {
+  path: '/public',
+  element: <PublicLayout />,
+  children: [
+    {
+      path: '/public/dashboard',
+      element: <DashboardDefault />
+    },
+    {
+      path: 'dashboard',
+      children: [
+        {
+          path: 'default',
+          element: <DashboardDefault />
+        }
+      ]
+    },
+    {
+      path: 'typography',
+      element: <Typography />
+    },
+    {
+      path: 'color',
+      element: <Color />
+    },
+    {
+      path: 'shadow',
+      element: <Shadow />
+    },
+    {
+      path: '/public/sample-page',
+      element: <SamplePage />
+    }
+  ]
+};
+
+export default PublicRoutes;
---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+// project imports
+import MainRoutes from './MainRoutes';
+import PublicRoutes from './PublicRoutes';
+import LoginRoutes from './LoginRoutes';
+
+// ==============================|| ROUTING RENDER ||============================== //
+
+const router = createBrowserRouter([MainRoutes, PublicRoutes, LoginRoutes], { basename: import.meta.env.VITE_APP_BASE_NAME });
+
+export default router;
